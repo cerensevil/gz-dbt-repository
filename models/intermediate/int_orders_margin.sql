9d93bac394fd45d6fcce7c99cf4ac5befa27e91b
rabiaceren07@gmail.com <rabiaceren07@gmail.com>
2025-09-11 00:28:09 +0000

    sales margin

---
+++ models/intermediate/int_orders_margin.sql
@@ -0,0 +1,10 @@
+with sales_margin as (select * from {{ ref("int_sales_margin") }}),
+select
+    orders_id,
+    date_date,
+    sum(revenue) as total_revenue,
+    sum(quantity) as total_quantity,
+    sum(purchased_cost) as total_purchase_cost,
+    sum(margin) as total_margin
+from sales_margin
+group by orders_id, date_date
