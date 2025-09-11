c10f31b5e56429a3729c387b9a726aedfdfc8e5a
rabiaceren07@gmail.com <rabiaceren07@gmail.com>
2025-09-11 00:49:49 +0000

    intermediate models

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -7,8 +7,12 @@ ship as (
 select
     o.orders_id,
     o.date_date,
-    o.total_margin + s.shipping_fee - s.log_cost - s.ship_cost as operational_margin
+    o.total_margin 
+      + CAST(s.shipping_fee AS FLOAT64)
+      - CAST(s.logcost AS FLOAT64)
+      - CAST(s.ship_cost AS FLOAT64) as operational_margin
 from orders_margin o
 left join ship s
     on o.orders_id = s.orders_id
+
 
